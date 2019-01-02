c3e34670396a64d26cf9d9e230d2996aa083e614
Layne Gustafson <lgustaf1@binghamton.edu>
2019-01-02 18:31:24 -0500

    A white window

--- MGS_Sandbox/main.cpp
+++ MGS_Sandbox/main.cpp
@@ -7,10 +7,26 @@ const int WINDOW_WIDTH = 600, WINDOW_HEIGHT = 600;
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
 
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 8);
+
     SDL_Window* window = SDL_CreateWindow("MGS Sandbox", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
 
     SDL_GLContext gl_context = SDL_GL_CreateContext(window);
     SDL_GL_MakeCurrent(window, gl_context);
+
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, WINDOW_WIDTH, WINDOW_HEIGHT, 0, 0.0f, 0.5f);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    glEnable(GL_MULTISAMPLE);
 
     for (;;) {
         bool should_quit = false;
@@ -21,9 +37,20 @@ int main() {
                 should_quit = true;
                 break;
             }
+
+            if (event.type == SDL_KEYDOWN) {
+                if (event.key.keysym.sym == SDLK_ESCAPE) {
+                    should_quit = true;
+                    break;
+                }
+            }
         }
 
         if (should_quit) break;
+
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
 
         SDL_GL_SwapWindow(window);
     }
