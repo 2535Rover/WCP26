63d612d35c8cce2dfa27f4144d8963d5c6e21995
Layne Gustafson <lgustaf1@binghamton.edu>
2019-01-02 18:25:55 -0500

    Just a window

--- MGS_Sandbox/main.cpp
+++ MGS_Sandbox/main.cpp
@@ -2,6 +2,35 @@
 
 #include <SDL.h>
 
+const int WINDOW_WIDTH = 600, WINDOW_HEIGHT = 600;
+
 int main() {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow("MGS Sandbox", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
+
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    SDL_GL_MakeCurrent(window, gl_context);
+
+    for (;;) {
+        bool should_quit = false;
+
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                should_quit = true;
+                break;
+            }
+        }
+
+        if (should_quit) break;
+
+        SDL_GL_SwapWindow(window);
+    }
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
     return 0;
 }
