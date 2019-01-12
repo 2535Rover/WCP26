b9d13c7e015c0f326f703347848303504fa7dab5
Layne Gustafson <lgustaf1@binghamton.edu>
2019-01-12 13:12:44 -0500

    Switch to using the matrix stack for transformations

--- MGS_Sandbox/src/main.cpp
+++ MGS_Sandbox/src/main.cpp
@@ -6,7 +6,7 @@
 
 #include "grid.hpp"
 
-const int WINDOW_WIDTH = 600, WINDOW_HEIGHT = 600;
+const int WINDOW_WIDTH = 800, WINDOW_HEIGHT = 800;
 
 void fill_hex(int center_x, int center_y, int size) {
     glBegin(GL_POLYGON);
@@ -20,7 +20,7 @@ void fill_hex(int center_x, int center_y, int size) {
     glEnd();
 }
 
-void stroke_hex(int center_x, int center_y, int size, int stroke_width) {
+void stroke_hex(int stroke_width) {
     glLineWidth(stroke_width);
 
     glBegin(GL_LINE_LOOP);
@@ -28,23 +28,23 @@ void stroke_hex(int center_x, int center_y, int size, int stroke_width) {
     for (int i = 0; i < 6; i++) {
         float theta = (M_PI / 3.0f) * i;
 
-        glVertex2f(center_x + size * cosf(theta), center_y + size * sinf(theta));
+        glVertex2f(cosf(theta), sinf(theta));
     }
 
     glEnd();
 }
 
-void stroke_grid_outline(Grid* grid, float grid_size, float pixels_per_meter, float stroke_width) {
-    // We calculate the pixel size of a hexagon side as grid_size * pixels_per_meter.
-
-    float pixel_size = grid_size * pixels_per_meter;
+void stroke_grid_outline(Grid* grid, float stroke_width) {
 
     for (int q = -grid->offset; q <= grid->offset; q++) {
         for (int r = -grid->offset; r <= grid->offset; r++) {
-            float center_x = pixel_size * (3.0f/2.0f) * q;
-            float center_y = pixel_size * ((sqrtf(3.0f)/2.0f) * q + sqrtf(3.0f) * r);
+            glPushMatrix();
 
-            stroke_hex(center_x, center_y, pixel_size, stroke_width);
+            glTranslatef((3.0f/2.0f) * q, (sqrtf(3.0f)/2.0f) * q + sqrtf(3.0f) * r, 0.0f);
+
+            stroke_hex(stroke_width);
+
+            glPopMatrix();
         }
     }
 }
@@ -66,7 +66,7 @@ float lerp(float a, float b, float t) {
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 8);
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 16);
 
     SDL_Window* window = SDL_CreateWindow("MGS Sandbox", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
 
@@ -88,6 +88,8 @@ int main() {
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     glEnable(GL_MULTISAMPLE);
+
+    glDisable(GL_DEPTH_TEST);
 
     float grid_size = 1.0f; // Length of each hexagon side in meters.
 
@@ -177,13 +179,13 @@ int main() {
             }
 
             if (event.type == SDL_MOUSEBUTTONDOWN) {
-                if (event.button.button == SDL_BUTTON_LEFT) {
+                if (event.button.button == SDL_BUTTON_RIGHT) {
                     mouse_down = true;
                 }
             }
 
             if (event.type == SDL_MOUSEBUTTONUP) {
-                if (event.button.button == SDL_BUTTON_LEFT) {
+                if (event.button.button == SDL_BUTTON_RIGHT) {
                     mouse_down = false;
                 }
             }
@@ -239,11 +241,13 @@ int main() {
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
         glTranslatef(translate_x, translate_y, 0.0f);
+        glScalef(pixels_per_meter, pixels_per_meter, 1.0f);
+        glScalef(grid_size, grid_size, 1.0f);
 
-        float line_thickness = 20.0f * ((pixels_per_meter - MIN_PPM) / (MAX_PPM - MIN_PPM)) + 1.0f;
+        float line_thickness = 20.0f * ((pixels_per_meter - MIN_PPM) / (MAX_PPM - MIN_PPM)) + 2.0f;
 
         glColor3f(1.0f, 0.0f, 0.0f);
-        stroke_grid_outline(grid, grid_size, pixels_per_meter, line_thickness);
+        stroke_grid_outline(grid, line_thickness);
 
         SDL_GL_SwapWindow(window);
     }
